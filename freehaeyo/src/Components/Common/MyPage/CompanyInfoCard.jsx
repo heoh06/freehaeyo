030d720b252e698678fa5220ae6300400c68065f
heoh0613 <heoh0613@gmail.com>
2023-10-27 13:19:39 +0900

    feat:채용 올리기 페이지 마크업 완료 및 회사 마이페이지 안 컴포넌트 분리

---
+++ freehaeyo/src/Components/Common/MyPage/CompanyInfoCard.jsx
@@ -0,0 +1,15 @@
+import Tag from "../Tag";
+
+function CompanyInfoCard(){
+    return(
+        <div>
+            <p>회사소개</p>
+            <p>회사 소개 내용</p>
+            <ul>
+                <Tag/>
+            </ul>
+        </div>
+    )
+}
+
+export default CompanyInfoCard;

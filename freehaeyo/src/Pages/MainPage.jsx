d41194bb474735aca7550be3dacf46380200479d
heoh0613 <heoh0613@gmail.com>
2023-10-31 10:37:03 +0900

    feat:Mainpage 데이터 sliced 변수 추가 및 함수 간소화 수정

--- freehaeyo/src/Pages/MainPage.jsx
+++ freehaeyo/src/Pages/MainPage.jsx
@@ -6,12 +6,16 @@ import HireCard from "../Components/Common/HireCard";
 import Header from "../Components/Common/Header";
 import Footer from "../Components/Common/Footer";
 
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import HireData from "../MockData/HireData.json";
 import NameCardData from "../MockData/NameCardData.json";
 
 function MainPage() {
+
+    const slicedHireData = HireData.slice(0,4);
+    const slicedNameCardData = NameCardData.slice(0,4);
+
     return (
         <>
             <Header/>
@@ -41,17 +45,13 @@ function MainPage() {
                 <section>
                     <h3>신규채용</h3>
                     <ul>
-                        {HireData.map((employmentData)=>{
-                            return <HireCard employmentData={employmentData} key={employmentData.id}/>
-                        }).slice(0,4)}
+                        {slicedHireData.map((employmentData)=>(<HireCard employmentData={employmentData} key={employmentData.id}/>))}
                     </ul>
                 </section>
                 <section>
                     <h3>금주의 명함</h3>
                     <ul>
-                        {NameCardData.map((data)=>{
-                            return <NameCard userData={data} key={data.id}/>
-                        }).slice(0,4)}
+                        {slicedNameCardData.map((data)=>(<NameCard userData={data} key={data.id}/>))}
                     </ul>
                 </section>
             </main>

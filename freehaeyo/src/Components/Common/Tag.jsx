82bcb17459b4b6e1bd6829d4dc059fe82021cc3a
heoh0613 <heoh0613@gmail.com>
2023-11-01 13:30:28 +0900

    feat:Tag props 추가, tag type 분기, 버튼 타입 태그 기능 구현

--- freehaeyo/src/Components/Common/Tag.jsx
+++ freehaeyo/src/Components/Common/Tag.jsx
@@ -1,11 +1,31 @@
 import { Chip } from '@mantine/core';
+import styled from 'styled-components';
 
-function Tag({tag}){
+function Tag({tag, categoryData, type, handleTagClick}){
     return(
             <li>
-                <Chip>{tag}</Chip>
+                {type? 
+                <Chip>
+                    <ButtonStyled
+                    onClick={()=>handleTagClick(categoryData.id)}
+                    className={categoryData.isChecked? "selected" : ""}
+                    >
+                    {categoryData.category}
+                    </ButtonStyled> 
+                </Chip>
+                :<Chip>{tag}</Chip>}
             </li>
     )
 }
 
 export default Tag;
+
+// 임시 확인용 버튼 스타일, 삭제 필요
+const ButtonStyled = styled.button`
+    background-color:gray;
+    color:white;
+    &.selected{
+        background-color:blue;
+        color:blue;
+    }
+`

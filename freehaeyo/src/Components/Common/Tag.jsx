b9d97f50519f9b91889c49272b2b5f8ef2cf8fc0
heoh0613 <heoh0613@gmail.com>
2023-11-07 10:26:33 +0900

    feat: Tag 안 props 및 분기 삭제

--- freehaeyo/src/Components/Common/Tag.jsx
+++ freehaeyo/src/Components/Common/Tag.jsx
@@ -1,31 +1,9 @@
 import { Chip } from '@mantine/core';
-import styled from 'styled-components';
 
-function Tag({tag, categoryData, type, handleTagClick}){
+function Tag({tag}){
     return(
-            <li>
-                {type? 
-                <Chip>
-                    <ButtonStyled
-                    onClick={()=>handleTagClick(categoryData.id)}
-                    className={categoryData.isChecked? "selected" : ""}
-                    >
-                    {categoryData.category}
-                    </ButtonStyled> 
-                </Chip>
-                :<Chip>{tag}</Chip>}
-            </li>
+        <Chip value={tag}>{tag}</Chip>
     )
 }
 
 export default Tag;
-
-// 임시 확인용 버튼 스타일, 삭제 필요
-const ButtonStyled = styled.button`
-    background-color:gray;
-    color:white;
-    &.selected{
-        background-color:blue;
-        color:blue;
-    }
-`

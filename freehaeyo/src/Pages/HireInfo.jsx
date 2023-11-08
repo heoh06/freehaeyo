fdda763bc452ff23db19b6c8754fc3068cb65247
heoh0613 <heoh0613@gmail.com>
2023-11-08 12:24:07 +0900

    feat:hireInfo에 필요한 데이터 임포트 및 렌더 + 로그인/비로그인 분기

--- freehaeyo/src/Pages/HireInfo.jsx
+++ freehaeyo/src/Pages/HireInfo.jsx
@@ -3,49 +3,98 @@ import Alert from '../Assets/alert.svg';
 import Header from '../Components/Common/Header';
 import Footer from '../Components/Common/Footer';
 import NameCard from '../Components/Common/NameCard';
+import Tag from '../Components/Common/Tag';
 
-function HireInfo(){
-    return(
-        <>
-            <Header/>
-            <main>
-                <section>
-                    <div>
-                        <h3>제목</h3>
-                        <p>회사 이름</p>
-                        <p>지역</p>
-                    </div>
-                    <div>
-                        <p>채용 내용</p>
-                    </div>
-                    <div>
-                        <p>마감일</p>
-                        <span>YYYY.MM.DD</span>
-                    </div>
-                    <div>
-                        <img src={Alert} alt="알림"/>
-                        <div>
-                            <p>해당 채용에 허위 사실이 있을 경우, 프리해요팀에 알려주세요!</p>
-                            <span>freehaeyo@gmail.com</span>
-                        </div>
-                    </div>
-                </section>
-                <section>
-                    <NameCard/>
-                    <div>
-                        <button>명함 넣기</button>
-                        <button>북마크하기</button>
-                    </div>
-                    <div>
-                        <p>회사이름</p>
-                        <span>010 1111 1111</span>
-                        <p>e-mail</p>
-                    </div>
-                </section>
-            </main>
-            <Footer/>
-        </>
-    )
+import UserData from '../MockData/UserData.json';
+import HireData from '../MockData/HireData.json';
+import CompanyData from '../MockData/CompanyData.json';
+
+import { Link, useParams } from 'react-router-dom';
+
+function HireInfo() {
+  // Todo:나중에 로그인 상태관리로 가져오기
+  const isLoggined = false;
+  // Todo:currentUserId에 현재 유저 ID 넣고 나중에 상태관리로 바꾸기
+  const currentUserId = 1;
+  const currentUser = UserData.filter((user) => user.id === currentUserId)[0];
+
+  const hireParams = useParams();
+  const currentHireData = HireData[hireParams.id];
+
+  const currentCompanyId = currentHireData.companyId;
+  const companyData = CompanyData.filter(
+    (company) => company.id === currentCompanyId,
+  )[0];
+
+  const dueDate = new Date(currentHireData.createdAt).toLocaleDateString(
+    'ko-KR',
+    {
+      dateStyle: 'full',
+    },
+  );
+
+  return (
+    <>
+      <Header />
+      <main>
+        <section>
+          <div>
+            <div>
+              <h3>{currentHireData.title}</h3>
+              <p>{companyData.name}</p>
+              <p>{companyData.region}</p>
+            </div>
+            <div>
+              {currentHireData.stacks.map((tag, index) => (
+                <Tag tag={tag} key={index} />
+              ))}
+            </div>
+          </div>
+          <div>
+            <p>{currentHireData.content}</p>
+          </div>
+          <div>
+            <p>마감일</p>
+            <span>{dueDate}</span>
+          </div>
+          <div>
+            <img src={Alert} alt="알림" />
+            <div>
+              <p>해당 채용에 허위 사실이 있을 경우, 프리해요팀에 알려주세요!</p>
+              <span>freehaeyo@gmail.com</span>
+            </div>
+          </div>
+        </section>
+        <section>
+          {isLoggined ? (
+            <form>
+              <NameCard userData={currentUser} />
+              <div>
+                <button>명함넣기</button>
+                <button>북마크하기</button>
+              </div>
+            </form>
+          ) : (
+            <div>
+              <div>
+                <p>로그인 필요!</p>
+                <span>로그인을 해주세요</span>
+              </div>
+              <Link to="/login">
+                <button>로그인 하기</button>
+              </Link>
+            </div>
+          )}
+          <div>
+            <p>{companyData.name}</p>
+            <span>{companyData.phone}</span>
+            <p>{companyData.email}</p>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default HireInfo;

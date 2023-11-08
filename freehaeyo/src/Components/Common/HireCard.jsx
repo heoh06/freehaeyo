66e0f122951082600c76cc3b28712af4f90aac0c
heoh0613 <heoh0613@gmail.com>
2023-11-08 12:22:46 +0900

    feat: Company Data를 HireData로 대체 및 Link params 추가

--- freehaeyo/src/Components/Common/HireCard.jsx
+++ freehaeyo/src/Components/Common/HireCard.jsx
@@ -1,23 +1,31 @@
-import Tag from "./Tag";
+import Tag from './Tag';
+import CompanyData from '../../MockData/CompanyData.json';
 
+function HireCard({ employmentData }) {
+  const { title, createdAt, stacks, companyId } = employmentData;
 
-function HireCard({ employmentData }){
-    const {title, name, region, stack, createdAt} = employmentData;
-    return(
-        <li>
-            <ul>
-                <Tag tag={createdAt}></Tag>
-            </ul>
-            <div>
-                <p>{title}</p>
-                <p>{name}</p>
-                <p>{region}</p>
-            </div>
-            <ul>
-                {stack?.map((tag, index)=>(<Tag key={index} tag={tag}/>))}
-            </ul>
-        </li>
-    )
+  const currentCompanyData = CompanyData.filter(
+    (company) => company.id === companyId,
+  );
+  const { name, region } = currentCompanyData[0];
+
+  return (
+    <li>
+      <ul>
+        <Tag tag={createdAt}></Tag>
+      </ul>
+      <div>
+        <p>{title}</p>
+        <p>{name}</p>
+        <p>{region}</p>
+      </div>
+      <ul>
+        {stacks?.map((tag, index) => (
+          <Tag key={index} tag={tag} />
+        ))}
+      </ul>
+    </li>
+  );
 }
 
 export default HireCard;

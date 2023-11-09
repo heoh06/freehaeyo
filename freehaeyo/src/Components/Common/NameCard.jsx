da679596c15b4c2f103e128bf3f4ffe99e6e40f8
heoh0613 <heoh0613@gmail.com>
2023-11-09 14:37:02 +0900

    feat: NameCard 안에 Link 추가

--- freehaeyo/src/Components/Common/NameCard.jsx
+++ freehaeyo/src/Components/Common/NameCard.jsx
@@ -1,20 +1,25 @@
-import Tag from "./Tag"
+import Tag from './Tag';
+import { Link } from 'react-router-dom';
 
-function NameCard({ userData }){
-    const {name, phone, email, stack} = userData;
+function NameCard({ userData }) {
+  const { id, name, phone, email, stack } = userData;
 
-    return(
-        <li>
-            <div>
-                <p>{name}</p>
-                <p>{phone}</p>
-                <p>{email}</p>
-            </div>
-            <ul>
-                {stack?.map((tag, index)=>(<Tag key={index} tag={tag}/>))}
-            </ul>
-        </li>
-    )
+  return (
+    <li>
+      <Link to={`/userinfo/${id}`}>
+        <div>
+          <p>{name}</p>
+          <p>{phone}</p>
+          <p>{email}</p>
+        </div>
+        <ul>
+          {stack?.map((tag, index) => (
+            <Tag key={index} tag={tag} />
+          ))}
+        </ul>
+      </Link>
+    </li>
+  );
 }
 
 export default NameCard;

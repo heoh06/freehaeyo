561ad6ad463bc099d8816eabf7fe09466f13a608
heoh0613 <heoh0613@gmail.com>
2023-11-17 12:26:01 +0900

    feat: 정보 카테고리에 다라 문구 달라지게 변경

--- freehaeyo/src/Components/Common/MyPage/NoContentBox.jsx
+++ freehaeyo/src/Components/Common/MyPage/NoContentBox.jsx
@@ -1,11 +1,18 @@
-function NoContentBox(){
-    return(
-        <div>
-            <p>아직 명함을 넣은 회사가 없어요!</p>
-            <span>채용 중인 회사들을 보시겠어요?</span>
-            <button>채용 중인 회사 보기</button>
-        </div>
-    )
+import { Link } from 'react-router-dom';
+
+function NoContentBox({ text }) {
+  const { title, subtitle, buttonPhrase, buttonLink } = text;
+  return (
+    <>
+      <div>
+        <p>{title}</p>
+        <span>{subtitle}</span>
+        <button>
+          <Link to={buttonLink}>{buttonPhrase}</Link>
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default NoContentBox;

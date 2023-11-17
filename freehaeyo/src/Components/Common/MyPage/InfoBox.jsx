284cafeb04680869d439469fadae50024aafbf20
heoh0613 <heoh0613@gmail.com>
2023-11-17 12:24:53 +0900

    feat:카테고리별 문구 변화, 데이터 전달

--- freehaeyo/src/Components/Common/MyPage/InfoBox.jsx
+++ freehaeyo/src/Components/Common/MyPage/InfoBox.jsx
@@ -1,29 +1,44 @@
-import NoContentBox from "./NoContentBox";
-import HireSummaryCard from "./HireSummaryCard";
-import HiringSummaryCard from "./HiringSummaryCard";
+import NoContentBox from './NoContentBox';
+import HireSummaryCard from './HireSummaryCard';
+import HireCard from '../HireCard';
 
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function InfoBox(){
-    return(
+import MyPageText from '../../../Assets/MyPageText';
+import HireData from '../../../MockData/HireData.json';
+
+function InfoBox({ data, infoCategory }) {
+  const { title, noContent, category } = MyPageText.filter(
+    (data) => data.category === infoCategory,
+  )[0];
+
+  return (
+    <div>
+      <div>
+        <p>{title}</p>
+        <span>12</span>
+      </div>
+      {category === 'hiring' ? (
         <div>
-            <div>
-                <p>명함 넣은 회사</p>
-                <span>12</span>
-            </div>
-            <div>
-                <p><Link to="/createhire">+ 채용 추가하기</Link></p>
-            </div>
-            <div>
-                <NoContentBox/>
-                <ul>
-                    <HireSummaryCard/>
-                    <HiringSummaryCard/>
-                </ul>
-                <div/>
-            </div>
+          <Link to="/createhire">+ 채용추가하기</Link>
         </div>
-    )
+      ) : null}
+      <div>
+        <NoContentBox text={noContent} />
+        <ul>
+          {/* Todo: HireData를 LocalStorage에 있는 데이터로 변경하기 */}
+          {category === 'hiring' || 'pastHire'
+            ? HireData.map((data) => (
+                <HireSummaryCard employmentData={data} key={data.id} />
+              ))
+            : HireData.map((data) => (
+                <HireCard employmentData={data} key={data.id} />
+              ))}
+        </ul>
+        <div />
+      </div>
+    </div>
+  );
 }
 
 export default InfoBox;

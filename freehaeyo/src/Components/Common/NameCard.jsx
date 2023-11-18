8e4d2aaef75b81326168ef839e8651dcdb1c6a29
heoh0613 <heoh0613@gmail.com>
2023-11-18 21:06:33 +0900

    feat: NameCard에 checkbox 타입 추가 및 input 태그와 함수 추가

--- freehaeyo/src/Components/Common/NameCard.jsx
+++ freehaeyo/src/Components/Common/NameCard.jsx
@@ -1,20 +1,30 @@
-import Tag from "./Tag"
+import Tag from './Tag';
 
-function NameCard({ userData }){
-    const {name, phone, email, stack} = userData;
+function NameCard({ userData, checkbox, onChangeCheckBox }) {
+  const { id, name, phone, email, stack } = userData;
 
-    return(
-        <li>
-            <div>
-                <p>{name}</p>
-                <p>{phone}</p>
-                <p>{email}</p>
-            </div>
-            <ul>
-                {stack?.map((tag, index)=>(<Tag key={index} tag={tag}/>))}
-            </ul>
-        </li>
-    )
+  return (
+    <li>
+      <div>
+        <p>{name}</p>
+        <p>{phone}</p>
+        <p>{email}</p>
+      </div>
+      {checkbox ? (
+        <input
+          type="checkbox"
+          onChange={(e) => {
+            onChangeCheckBox(e.target.checked, id);
+          }}
+        ></input>
+      ) : null}
+      <ul>
+        {stack?.map((tag, index) => (
+          <Tag key={index} tag={tag} />
+        ))}
+      </ul>
+    </li>
+  );
 }
 
 export default NameCard;

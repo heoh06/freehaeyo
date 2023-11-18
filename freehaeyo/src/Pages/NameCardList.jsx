55fd9965719b0463d668a26c47e4d1144834dfdb
heoh0613 <heoh0613@gmail.com>
2023-11-18 21:07:04 +0900

    feat: 명함 리스트 페이지에 체크한 명함 데이터 모으는 변수 추가

--- freehaeyo/src/Pages/NameCardList.jsx
+++ freehaeyo/src/Pages/NameCardList.jsx
@@ -1,24 +1,53 @@
-import NameCard from "../Components/Common/NameCard";
+import Header from '../Components/Common/Header';
+import NameCard from '../Components/Common/NameCard';
 
-function NameCardList(){
-    return(
-        <>
-            <div>
-                <p>받은 명함</p>
-                <div>
-                    <form>
-                        <button>명함 채택하기</button>
-                    </form>
-                </div>
-                <main>
-                    <p>받은 명함이 아직 없어요!</p>
-                    <ul>
-                        <NameCard/>
-                    </ul>
-                </main>
-            </div>
-        </>
-    )
+import UserData from '../MockData/UserData.json';
+
+import { useState } from 'react';
+
+function NameCardList() {
+  // Todo: 나중에 채용 공고 리스트에 들어온 userid 찾아서 돌리기
+
+  const [checkedUserId, setCheckedUserId] = useState([]);
+
+  function onChangeCheckBox(checked, userId) {
+    if (checked) {
+      setCheckedUserId([...checkedUserId, userId]);
+    }
+  }
+
+  //Todo: submit 버튼 누르고 hireData에 Post 시 보내는 데이터
+  const checkedUserIdList = { checkedNameCard: checkedUserId };
+
+  return (
+    <>
+      <Header />
+      <div>
+        <h3>받은 명함</h3>
+        <div>
+          <form>
+            <button>명함 채택하기</button>
+          </form>
+        </div>
+        <main>
+          {UserData ? (
+            <ul>
+              {UserData.map((data) => (
+                <NameCard
+                  userData={data}
+                  key={data.id}
+                  checkbox
+                  onChangeCheckBox={onChangeCheckBox}
+                />
+              ))}
+            </ul>
+          ) : (
+            <p>받은 명함이 아직 없어요!</p>
+          )}
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default NameCardList;

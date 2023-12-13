08196e8657d1c0871c215ed7bbc8b0125204998f
heoh0613 <heoh0613@gmail.com>
2023-12-13 13:35:00 +0900

    feat : msw 작업시키기 위한 setup

--- freehaeyo/src/main.jsx
+++ freehaeyo/src/main.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import { MantineProvider, createTheme } from '@mantine/core';
-import { createBrowserRouter, RouterProvider,} from "react-router-dom";
+import { worker } from './mocks/worker.js';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const theme = createTheme({
   /** Put your mantine theme override here */
 });
 
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MantineProvider theme={theme}>
+  await worker;
+
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <MantineProvider theme={theme}>
         <App />
-    </MantineProvider>
-  </React.StrictMode>,
-)
+      </MantineProvider>
+    </React.StrictMode>,
+  );
+});

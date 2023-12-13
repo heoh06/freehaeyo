b9eef4e08fef3e4d776291bfb616a3ef0563c639
heoh0613 <heoh0613@gmail.com>
2023-12-13 13:32:51 +0900

    chore : NameCard 안에 stacks를 resume로 변경

--- freehaeyo/src/Components/Common/NameCard.jsx
+++ freehaeyo/src/Components/Common/NameCard.jsx
@@ -1,31 +1,30 @@
 import Tag from './Tag';
 import { Link } from 'react-router-dom';
 
-
 function NameCard({ userData, checkbox, onChangeCheckBox }) {
-  const { id, name, phone, email, stack } = userData;
+  const { id, name, phone, email, resume } = userData;
 
   return (
     <li>
-    <Link to={`/userinfo/${id}`}>
-      <div>
-        <p>{name}</p>
-        <p>{phone}</p>
-        <p>{email}</p>
-      </div>
-      {checkbox ? (
-        <input
-          type="checkbox"
-          onChange={(e) => {
-            onChangeCheckBox(e.target.checked, id);
-          }}
-        ></input>
-      ) : null}
-      <ul>
-        {stack?.map((tag, index) => (
-          <Tag key={index} tag={tag} />
-        ))}
-      </ul>
+      <Link to={`/userinfo/${id}`}>
+        <div>
+          <p>{name}</p>
+          <p>{phone}</p>
+          <p>{email}</p>
+        </div>
+        {checkbox ? (
+          <input
+            type="checkbox"
+            onChange={(e) => {
+              onChangeCheckBox(e.target.checked, id);
+            }}
+          ></input>
+        ) : null}
+        <ul>
+          {resume?.map((tag, index) => (
+            <Tag key={index} tag={tag} />
+          ))}
+        </ul>
       </Link>
     </li>
   );
